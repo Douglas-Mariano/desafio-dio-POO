436df5d687dc5590ccc5882524d76dc130ab74f9
douglasmariano <douglas.arm@live.com>
2022-07-29 21:07:51 -0300

    Criando Class Conteudo

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,10 +2,8 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo {
 	
-	private String titulo;
-	private String descricao;
 	private LocalDate data;
 	
 	public Mentoria() {
@@ -13,24 +11,8 @@ public class Mentoria {
 	}
 
 	@Override
-	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
-	}
-
-	public String getTitulo() {
-		return titulo;
-	}
-
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public double calcularXP() {
+		return XP_PADRAO + 20d;
 	}
 
 	public LocalDate getData() {
@@ -39,6 +21,14 @@ public class Mentoria {
 
 	public void setData(LocalDate data) {
 		this.data = data;
+	}
+	
+	@Override
+	public String toString() {
+		return "Mentoria: " + 
+				"Titulo: " + getTitulo() + 
+				" / " + "Descricao: " + getDescricao() + 
+				" / " + "Data: " + data;
 	}
 	
 }
